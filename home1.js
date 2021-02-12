34ac3e7aa7cf1732582eab88a2cdb7ebf2e29820
SHIVAMTYAGI2309 <64794844+SHIVAMTYAGI2309@users.noreply.github.com>
2021-02-12 11:05:52 +0530

    Update home1.js

--- home1.js
+++ home1.js
@@ -1,1 +1,151 @@
-harray(),setRemainingPathColor(timeLeft),0===timeLeft&&onTimesUp()},1e3)}function formatTime(e){let t=e%60;return t<10&&(t=`0${t}`),`${Math.floor(e/60)}:${t}`}function setRemainingPathColor(e){const{alert:t,warning:n,info:a}=COLOR_CODES;e<=t.threshold?(document.getElementById("base-timer-path-remaining").classList.remove(n.color),document.getElementById("base-timer-path-remaining").classList.add(t.color)):e<=n.threshold&&(document.getElementById("base-timer-path-remaining").classList.remove(a.color),document.getElementById("base-timer-path-remaining").classList.add(n.color))}function calculateTimeFraction(){const e=timeLeft/TIME_LIMIT;return e-1/TIME_LIMIT*(1-e)}function setCircleDasharray(){const e=`${(calculateTimeFraction()*FULL_DASH_ARRAY).toFixed(0)} 283`;document.getElementById("base-timer-path-remaining").setAttribute("stroke-dasharray",e)}document.getElementById("app").innerHTML=`\n<div class="base-timer">\n  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">\n    <g class="base-timer__circle">\n      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>\n      <path\n        id="base-timer-path-remaining"\n        stroke-dasharray="283"\n        class="base-timer__path-remaining ${remainingPathColor}"\n        d="\n          M 50, 50\n          m -45, 0\n          a 45,45 0 1,0 90,0\n          a 45,45 0 1,0 -90,0\n        "\n      ></path>\n    </g>\n  </svg>\n  <span id="base-timer-label" class="base-timer__label">${formatTime(timeLeft)}</span>\n</div>\n`,startTimer();var numberofbuttons=document.querySelectorAll(".btn").length;for(i=0;i<numberofbuttons;i++)document.querySelectorAll(".btn")[i].addEventListener("click",function(){switch(this.innerHTML){case"BIRD'S EYE":new Audio("./sounds/Birds-chirping-sound-effect.mp3").play(),document.getElementById("x").src="bird-eye.mp4";break;case"RAINFALL":new Audio("./sounds/rain.mp3").play(),document.getElementById("x").src="Raindrops_Videvo.mp4";break;case"BEACH":new Audio("./sounds/beach.mp3").play(),document.getElementById("x").src="15.mp4";break;case"MOUNTAINS":new Audio("./sounds/mount.mp3").play(),document.getElementById("x").src="mount.mp4"}});
+
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+
+const TIME_LIMIT = 120;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+document.getElementById("app").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">${formatTime(
+    timeLeft
+  )}</span>
+</div>
+`;
+
+startTimer();
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML = formatTime(
+      timeLeft
+    );
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+
+var numberofbuttons=document.querySelectorAll(".btn").length;
+for(i=0;i<numberofbuttons;i++){
+  document.querySelectorAll(".btn")[i].addEventListener("click",function(){
+    var sound=this.innerHTML;
+    switch (sound) {
+      case "BIRD'S EYE":
+
+        var audio= new Audio("./sounds/Birds-chirping-sound-effect.mp3");
+        audio.play();
+       var vid=document.getElementById("x");
+        vid.src="bird-eye.mp4";
+
+        break;
+      case "RAINFALL":
+      var audio= new Audio("./sounds/rain.mp3");
+      audio.play();
+       var vid=document.getElementById("x");
+        vid.src="Raindrops_Videvo.mp4";
+        break;
+        case "BEACH":
+
+        var audio= new Audio("./sounds/beach.mp3");
+        audio.play();
+         var vid=document.getElementById("x");
+          vid.src="15.mp4";
+          break;
+          case "MOUNTAINS":
+          var audio= new Audio("./sounds/mount.mp3");
+          audio.play();
+           var vid=document.getElementById("x");
+            vid.src="mount.mp4";
+            break;
+      default:
+    }
+  });
+}

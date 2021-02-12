4966e1f25fed02e5eae8ecd80e61ba6e32f1e0bc
SHIVAMTYAGI2309 <64794844+SHIVAMTYAGI2309@users.noreply.github.com>
2021-02-12 10:03:46 +0530

    Update home3.js

--- home3.js
+++ home3.js
@@ -121,25 +121,25 @@ for(i=0;i<numberofbuttons;i++){
         var audio= new Audio("./sounds/Birds-chirping-sound-effect.mp3");
         audio.play();
        var vid=document.getElementById("x");
-        vid.src="./videos/bird eye.mp4";
+        vid.src="bird-eye.mp4";
         break;
       case "RAINFALL":
       var audio= new Audio("./sounds/rain.mp3");
       audio.play();
        var vid=document.getElementById("x");
-        vid.src="./videos/Raindrops_Videvo.mp4";
+        vid.src="Raindrops_Videvo.mp4";
         break;
         case "BEACH":
         var audio= new Audio("./sounds/beach.mp3");
         audio.play();
          var vid=document.getElementById("x");
-          vid.src="./videos/15.mp4";
+          vid.src="15.mp4";
           break;
           case "MOUNTAINS":
           var audio= new Audio("./sounds/mount.mp3");
           audio.play();
            var vid=document.getElementById("x");
-            vid.src="./videos/mount.mp4";
+            vid.src="mount.mp4";
             break;
       default:
     }
